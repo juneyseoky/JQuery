61768a34c0f6d15e77feaef266d7e4805b98f0f8
juneyseoky <124223984+juneyseoky@users.noreply.github.com>
2024-10-07 17:36:36 +0900

    jQuery mainPage

--- p05/quiz/script/script.js
+++ p05/quiz/script/script.js
@@ -1,16 +1,10 @@
-$(function () {
-    $("h2").css({
-        "color": "#08f",
-        "border": "3px solid #08f"
-    }).siblings().css({
-        "color": "#f00",
-        "border": "3px solid #f00"
-    }).add("span").css("display", "block").parent().css({
-        "width": "640px",
-        "padding": "20px",
-        "border": "2px solid #08f",
-        "border-radius": "8px",
-        "color": "#08f",
-        "margin": "20px auto"
-    }).prepend("div")
+const btnDom = document.querySelector("button");
+let insVal; // 입력 숫자
+let res = ``;
+btnDom.addEventListener("click", function () {
+  insVal = document.querySelector("#insVal").value;
+  let i = 2;
+  for (; i <= insVal; i++) if (insVal % i == 0) break;
+  res = `${insVal}은(는) ${i}의 배수입니다.`;
+  document.querySelector("#res").innerText = res;
 });
